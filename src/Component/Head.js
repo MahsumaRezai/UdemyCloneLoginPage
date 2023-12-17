2e2b78b8d42dcbb1c9111c0d5b96ef89cadf289b
unknown <mahsuma.rezai8@gmail.com>
2023-12-17 06:26:59 -0800

    add style head

--- src/Component/Head.js
+++ src/Component/Head.js
@@ -1,17 +1,22 @@
 
 import { Fragment } from 'react';
 import classes from './Head.module.css'
+import head from './image/head.PNG'
 
 const Head = (props) => {
     return (
         <Fragment>
             <div className={classes.head}>
+                <div className={classes.headimage} >
+                    <img alt="head" src={head}  />
+
+            </div>
 
             </div>
 
 
 
-        </Fragment>
+        </Fragment >
     );
 };
 
